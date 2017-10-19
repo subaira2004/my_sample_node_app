62d6a96ebff6acdf6bb603cfdd427345b25472d1
Ahamed Zubair Ali <subair_a2005@hotmail.com>
2017-10-19 14:52:19 +0400

    Update index.js

--- app_modules/users/index.js
+++ app_modules/users/index.js
@@ -1,6 +1,6 @@
 
 var users = (req, res) => {
     var users = [{ name: 'aaa', age: 43 }, { name: 'bbb', age: 23 }]
-    res.render('users', { users: users });
+    res.render('users/users', { users: users });
 }
 module.exports = users;
