1d990f570ee826be04ec2ed983f3e6eec2b03b3a
subaira2004 <subair_a2005@hotmail.com>
2018-03-11 18:20:05 +0400

    app in progress with error

---
+++ src/user/user.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: props.users
+        };
+
+        render()
+        {
+            return(
+                <div class="row">
+                    <div class="row">
+                        <input type="button" class="btn btrn-primary" value="New" />
+                    </div>
+                    <div class="row">
+                    if(this.state.users && this.state.users.length>0)
+                    {
+                        <table  class="table table-default table-striped table-bordered" style='width:100%;border:1px solid;border-collapse:collapse'>
+                        <tr style="background-color:#888;color:white;">
+                            <th>Name</th>
+                            <th>age</th>
+                            <th>department</th>
+                            <th>designation</th>
+                            <th>Action</th>
+                        </tr>  
+                        this.state.users.map((user)=>
+                            <tr>
+                                <td>
+                                    <a href="/users/"{user.name} > {user.name}</a>
+                                </td>
+                                <td>
+                                    {user.age}
+                                </td>
+                                <td>
+                                    {user.department}
+                                </td>
+                                <td>
+                                    {user.designation}
+                                </td>
+                                <td>
+                                    <a  href="/users/edit/"{user.name} class="btn btn-primary" > Edit</a>&nbsp;
+                                    <a href="/users/delete/"{user.name} class="btn btn-primary"> delete</a>
+                                </td>
+                            </tr>
+                        );
+                        </table>
+                    }
+                    else
+                    {
+                        <span class="alert alert-danger">No Users to show!</span>
+                    }
+                    </div>
+                </div>
+            );
+        }
+
+    }
+}
+
+ReactDOM.render(<User users={users}/>,document.getElementById("userApp"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,34 @@
 const path = require('path');
 
-module.exports = {
-  mode:"development",
+const homeConfigs = {
+  mode: "development",
   entry: './src/home.js',
   module: {
-   rules: [{test:/\.(js|jsx)$/,use:"babel-loader"}]
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: "babel-loader"
+    }]
   },
   output: {
     filename: 'home.js',
     path: path.resolve(__dirname, 'public')
   }
 };
+
+
+const userConfigs = {
+  mode: "development",
+  entry: './src/user/user.js',
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: "babel-loader"
+    }]
+  },
+  output: {
+    filename: 'user.js',
+    path: path.resolve(__dirname, 'views/users/js')
+  }
+};
+
+module.exports = [homeConfigs, userConfigs];
